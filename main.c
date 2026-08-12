ff020ebf071a439efcbf73d0f188436167d57bcf
based-watermelon <nilayagokula@gmail.com>
2026-08-12 17:07:52 +0530

    feat: current player switching, mouseclick and sloppy grid definition

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 typedef struct{
     int board[3][3];
@@ -12,13 +13,10 @@ typedef struct{
 int main(void){
     
     // initialize
-    Game ng;
+    Game ng = {0};
     const int W_WIDTH = 400;
     const int W_HEIGHT = 450;
-    ng.board;
     ng.current_player = 1;
-    ng.game_won = false;
-    ng.winning_player = 0;
 
 
     InitWindow(W_WIDTH, W_HEIGHT, "Tic-Tac-Toe");
@@ -28,6 +26,24 @@ int main(void){
     //main game loop
     while(!(WindowShouldClose())){
         
+        Vector2 mousePos = GetMousePosition();
+
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            if (mousePos.x < 40 + 320.0/3 && mousePos.x > 0 && mousePos.y > 115 + 320.0/3 && mousePos.y < 115 + 2*(320.0/3)){
+                if(ng.current_player == 1 && ng.board[0][0] == 0){
+                    ng.board[0][0] = 1;
+                }
+                else if(ng.current_player == -1 && ng.board[0][0] == 0){
+                    ng.board[0][0] = -1;
+                }
+                ng.current_player *= -1;
+                }
+
+
+            
+            }
+        
+        printf("pos 1 = %d player = %d\n",ng.board[0][0], ng.current_player);
 
 
 
@@ -36,7 +52,11 @@ int main(void){
         BeginDrawing();
             ClearBackground(RAYWHITE);
             // DrawText("New Window! ", W_WIDTH/2 - 20, W_HEIGHT/2, 20, BLACK);
+
+            DrawText(TextFormat("Mouse X: %.0f", mousePos.x), 20, 20, 20, BLACK);
+            DrawText(TextFormat("Mouse Y: %.0f", mousePos.y), 20, 50, 20, BLACK);
         EndDrawing();
+        
     }
 
     CloseWindow();
