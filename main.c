7ceb1377258bcb00a1119ee58608dc564c889d7f
based-watermelon <nilayagokula@gmail.com>
2026-08-17 15:13:50 +0530

    feat: Added Win condition

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include "raylib.h"
-#include "raymath.h"
 #include <stdbool.h>
 #include <stdio.h>
 
 
 #define CELL_SIZE  (320.0f / 3.0f)
+
 typedef struct{
     int board[3][3];
     int current_player;
-    bool game_won;
+    int game_won;
     int winning_player;
 } Game;
 
@@ -37,6 +37,23 @@ int move_made(float x, float y, int board[3][3], int *curr_player, float cs){
                 return  1;
                 }
 
+int win_pattern(int board[3][3], int curr_player){
+    for(int i=0;i<3;i++){
+        int a = board[i][0], b=board[i][1], c=board[i][2];
+        if(a==b && b==c && a!=0){
+            return curr_player;
+        }
+    for(int j=0;j<3;j++){
+        int a = board[0][j], b=board[1][j], c=board[2][j];
+        if(a==b && b==c && a!=0){
+            return curr_player;
+        }
+    int d1 = board[0][0], d2 = board[1][1], d3 = board[2][2], d4 = board[2][0], d5 = board[0][2];
+    if ((d1==d2 && d2==d3 && d1!=0) || (d4==d2 && d2==d5 && d4!=0))
+        return curr_player;
+        }
+    }
+}
 
 int main(void){
     
@@ -57,21 +74,30 @@ int main(void){
         Vector2 mousePos = GetMousePosition();
 
         // game logic
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        ng.game_won = win_pattern ( ng.board, ng.current_player);
+
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && ng.game_won == 0){
             move_made(mousePos.x, mousePos.y, ng.board , &ng.current_player, CELL_SIZE);
-        
-        printf("player = %d\n",ng.current_player);
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
                 printf("%d ", ng.board[i][j]);
             }
             printf("\n");
-        }
+                
+        }}
+        
         //rendering
         BeginDrawing();
             ClearBackground(RAYWHITE);
             // DrawText("New Window! ", W_WIDTH/2 - 20, W_HEIGHT/2, 20, BLACK);
 
+            if(ng.game_won == 1){
+                DrawText(TextFormat("Player 2 has won the match"), 20, 80, 20, BLACK);
+                }
+            if(ng.game_won == -1){
+                DrawText(TextFormat("Player 1 has won the match"), 20, 80, 20, BLACK);
+                }
+            
             DrawText(TextFormat("Mouse X: %.0f", mousePos.x), 20, 20, 20, BLACK);
             DrawText(TextFormat("Mouse Y: %.0f", mousePos.y), 20, 50, 20, BLACK);
         EndDrawing();
